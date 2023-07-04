a3e327471390109c9d0a6d5c72cba5d2c4ce8182
chipa <mezismezis@gmail.com>
2023-07-04 14:54:18 +0300

    feat(models): creat basis models
    Created Topic model with name field. Created Redactor model with AbstractUser and one additional years_of_experience field. Create Newspaper model with title, content, published_date, topic as ManyToManyField to Topic model and publishers as ManyToManyField to Redactor model.

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        ordering = ["username"]
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topic = models.ManyToManyField(Topic, related_name="topics")
+    publishers = models.ManyToManyField(Redactor, related_name="redactors")
